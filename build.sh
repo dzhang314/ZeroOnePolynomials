3aaf249f6bc59a82a5d4e17e82c57cc1e4967589
David K. Zhang <dzhang314@gmail.com>
2023-05-15 02:10:43 -0700

    rename DeepEquationReducer to EquationReducer

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 clang++ -std=c++20 -Weverything -Wno-c++98-compat -Wno-unsafe-buffer-usage \
-    -O3 -march=native DeepEquationReducer.cpp -o DeepEquationReducer
+    -O3 -march=native EquationReducer.cpp -o EquationReducer
