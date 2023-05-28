c05aabc45cb1f4d6d856b69d46c352e2975a1a71
David K. Zhang <dzhang314@gmail.com>
2023-05-28 02:42:48 -0700

    disable some irrelevant warnings

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 mkdir -p bin
 
 clang++ -std=c++20 -Weverything -Wno-c++98-compat -Wno-c++98-compat-pedantic \
+    -Wno-poison-system-directories -Wno-unsafe-buffer-usage \
     -O3 -march=native -flto \
     src/Term.cpp src/Polynomial.cpp src/ZeroSubstitution.cpp src/System.cpp \
     src/Assertions.cpp src/Simplification.cpp src/ZeroOneSolver.cpp \
