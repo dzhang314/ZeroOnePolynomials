3de01bfb8eaaa3919c0af1f77eb631e87a8f4fe9
David K. Zhang <dzhang314@gmail.com>
2023-11-15 16:13:31 -0800

    Update ParallelSolver.py

--- ParallelSolver.py
+++ ParallelSolver.py
@@ -8,8 +8,8 @@ import sys
 import time
 
 
-def pair_iterator():
-    for k in itertools.count():
+def pair_iterator(n: int | None):
+    for k in itertools.count() if n is None else range(n + 1):
         for j in range(k + 1):
             i = k - j
             if i > j > 0:
@@ -23,12 +23,31 @@ def get_num_cores() -> int:
     return result - 1  # reserve one process for this program, desktop UI, etc.
 
 
+def wait_for_process_to_finish(
+    processes: list[tuple[subprocess.Popen[bytes], io.TextIOWrapper, str, str]]
+):
+    while True:
+        for k in range(len(processes)):
+            return_code = processes[k][0].poll()
+            if return_code is not None:
+                _, file, src, dst = processes[k]
+                if return_code == 0:
+                    print("Finished computation of", dst + ".")
+                else:
+                    print("ERROR: Process", dst, "returned non-zero exit code.")
+                del processes[k]
+                file.close()
+                os.rename(src, dst)
+                return None
+        time.sleep(0.125)
+
+
 def main():
     if not os.path.isdir("data"):
         os.mkdir("data")
     processes: list[tuple[subprocess.Popen[bytes], io.TextIOWrapper, str, str]] = []
     num_processes = int(sys.argv[1]) if len(sys.argv) > 1 else get_num_cores()
-    for i, j in pair_iterator():
+    for i, j in pair_iterator(int(sys.argv[2]) if len(sys.argv) > 2 else None):
         file_name = f"data/ZeroOneEquations_{i:04}_{j:04}.txt"
         # if this data file has not already been computed...
         if os.path.isfile(file_name):
@@ -36,22 +55,7 @@ def main():
         else:
             # if we have hit the process limit, wait for a process to finish
             while len(processes) >= num_processes:
-                found = False
-                for k in range(len(processes)):
-                    return_code = processes[k][0].poll()
-                    if return_code is not None:
-                        _, file, src, dst = processes[k]
-                        found = True
-                        if return_code == 0:
-                            print("Finished computation of", dst + ".")
-                        else:
-                            print("ERROR: Process", dst, "returned non-zero exit code.")
-                        del processes[k]
-                        file.close()
-                        os.rename(src, dst)
-                        break
-                if not found:
-                    time.sleep(0.25)
+                wait_for_process_to_finish(processes)
             # now that a slot is available, start a process to compute this file
             temp_name = file_name + "." + str(i + j) + ".temp"
             file = open(temp_name, "w+")
@@ -66,6 +70,8 @@ def main():
                     file_name,
                 )
             )
+    while processes:
+        wait_for_process_to_finish(processes)
 
 
 if __name__ == "__main__":
