0a38edb91259c56aaf5d6fc6e50af5fe392e74df
David K. Zhang <dzhang314@gmail.com>
2023-11-19 12:18:45 -0800

    add macos build and bench scripts

---
+++ time_macos.sh
@@ -0,0 +1,20 @@
+time bin/ZeroOneSolverClangO1 20 15 > /dev/null
+time bin/ZeroOneSolverClangLTO1 20 15 > /dev/null
+time bin/ZeroOneSolverClangO2 20 15 > /dev/null
+time bin/ZeroOneSolverClangLTO2 20 15 > /dev/null
+time bin/ZeroOneSolverClangO3 20 15 > /dev/null
+time bin/ZeroOneSolverClangLTO3 20 15 > /dev/null
+
+time bin/ZeroOneSolverHClangO1 20 15 > /dev/null
+time bin/ZeroOneSolverHClangLTO1 20 15 > /dev/null
+time bin/ZeroOneSolverHClangO2 20 15 > /dev/null
+time bin/ZeroOneSolverHClangLTO2 20 15 > /dev/null
+time bin/ZeroOneSolverHClangO3 20 15 > /dev/null
+time bin/ZeroOneSolverHClangLTO3 20 15 > /dev/null
+
+time bin/ZeroOneSolverGCCO1 20 15 > /dev/null
+time bin/ZeroOneSolverGCCLTO1 20 15 > /dev/null
+time bin/ZeroOneSolverGCCO2 20 15 > /dev/null
+time bin/ZeroOneSolverGCCLTO2 20 15 > /dev/null
+time bin/ZeroOneSolverGCCO3 20 15 > /dev/null
+time bin/ZeroOneSolverGCCLTO3 20 15 > /dev/null
