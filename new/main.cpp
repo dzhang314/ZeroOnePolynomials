01873ea1de06bd95c32c51af772683ea1cc27be3
David K. Zhang <dzhang314@gmail.com>
2023-11-30 16:54:06 -0800

    fix simplification bug

--- new/main.cpp
+++ new/main.cpp
@@ -13,75 +13,6 @@ using ZeroOneSolver::VAR;
 using ZeroOneSolver::var_index_t;
 
 
-static std::ostream &operator<<(std::ostream &os, const Term &term) {
-    if (term == TERM_ZERO) {
-        os << "0";
-    } else {
-        if (term.p_index) {
-            os << "p" << static_cast<int>(term.p_index);
-            if (term.q_index) { os << "*q" << static_cast<int>(term.q_index); }
-        } else {
-            if (term.q_index) {
-                os << "q" << static_cast<int>(term.q_index);
-            } else {
-                os << "1";
-            }
-        }
-    }
-    return os;
-}
-
-
-template <var_index_t M, var_index_t N>
-static std::ostream &operator<<(std::ostream &os, const System<M, N> &system) {
-    std::bitset<M - 1> p_used;
-    std::bitset<N - 1> q_used;
-    for (std::size_t e = 0; e < M + N - 1; ++e) {
-        bool first = true;
-        for (std::size_t t = 0; t < M + 1; ++t) {
-            const Term term = system.lhs[e][t];
-            if (term == TERM_ZERO) { continue; }
-            if (term.p_index) { p_used.set(term.p_index - 1); }
-            if (term.q_index) { q_used.set(term.q_index - 1); }
-            if (first) {
-                first = false;
-            } else {
-                os << " + ";
-            }
-            os << term;
-        }
-        // Skip printing equations of the form 0 == 0.
-        if (first && (system.rhs.get(e) == RHS::ZERO)) { continue; }
-        switch (system.rhs.get(e)) {
-            case RHS::ZERO_OR_ONE: os << " == 0 or 1\n"; break;
-            case RHS::ZERO: os << " == 0\n"; break;
-            case RHS::ONE: os << " == 1\n"; break;
-        }
-    }
-    for (std::size_t i = 0; i < M - 1; ++i) {
-        const VAR value = system.p.get(i);
-        if ((value == VAR::ZERO) || (value == VAR::ONE)) {
-            assert(!p_used.test(i));
-        } else if (value == VAR::ZERO_OR_ONE) {
-            os << "p" << (i + 1) << " == 0 or 1\n";
-        } else {
-            os << "0 <= p" << (i + 1) << " <= 1\n";
-        }
-    }
-    for (std::size_t i = 0; i < N - 1; ++i) {
-        const VAR value = system.q.get(i);
-        if ((value == VAR::ZERO) || (value == VAR::ONE)) {
-            assert(!q_used.test(i));
-        } else if (value == VAR::ZERO_OR_ONE) {
-            os << "q" << (i + 1) << " == 0 or 1\n";
-        } else {
-            os << "0 <= q" << (i + 1) << " <= 1\n";
-        }
-    }
-    return os;
-}
-
-
 template <std::size_t N>
 constexpr bool increment(std::bitset<N> &bitset) noexcept {
     for (std::size_t i = 0; i < N; ++i) {
@@ -200,7 +131,6 @@ void analyze_case(const std::bitset<M - 1> &case_index) {
     while (!stack.empty()) {
         System<M, N> system = stack.back();
         stack.pop_back();
-        std::cout << system << std::endl;
         if (system.simplify()) {
             if (system.has_unknown_variable()) {
                 if (!find_case_split(stack, system)) {
