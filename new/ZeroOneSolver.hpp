6fa42ef96cef3b5b10f0c2ecf76501d1b719f29b
David K. Zhang <dzhang314@gmail.com>
2023-11-30 16:54:06 -0800

    fix simplification bug

--- new/ZeroOneSolver.hpp
+++ new/ZeroOneSolver.hpp
@@ -5,6 +5,7 @@
 #include <cassert> // for assert
 #include <cstddef> // for std::byte, std::size_t
 #include <cstdint> // for std::uint8_t
+#include <ostream> // for std::ostream
 
 namespace ZeroOneSolver {
 
@@ -36,6 +37,25 @@ struct Term {
 
 constexpr Term TERM_ZERO = {0xFF, 0xFF};
 constexpr Term TERM_ONE = {0x00, 0x00};
+
+
+inline std::ostream &operator<<(std::ostream &os, const Term &term) {
+    if (term == TERM_ZERO) {
+        os << "0";
+    } else {
+        if (term.p_index) {
+            os << "p" << static_cast<int>(term.p_index);
+            if (term.q_index) { os << "*q" << static_cast<int>(term.q_index); }
+        } else {
+            if (term.q_index) {
+                os << "q" << static_cast<int>(term.q_index);
+            } else {
+                os << "1";
+            }
+        }
+    }
+    return os;
+}
 
 
 template <typename T, std::size_t N>
@@ -140,6 +160,55 @@ struct System {
     }
 
 
+    friend std::ostream &operator<<(std::ostream &os, const System &system) {
+        std::bitset<M - 1> p_used;
+        std::bitset<N - 1> q_used;
+        for (std::size_t e = 0; e < M + N - 1; ++e) {
+            bool first = true;
+            for (std::size_t t = 0; t < M + 1; ++t) {
+                const Term term = system.lhs[e][t];
+                if (term == TERM_ZERO) { continue; }
+                if (term.p_index) { p_used.set(term.p_index - 1); }
+                if (term.q_index) { q_used.set(term.q_index - 1); }
+                if (first) {
+                    first = false;
+                } else {
+                    os << " + ";
+                }
+                os << term;
+            }
+            // Skip printing equations of the form 0 == 0.
+            if (first && (system.rhs.get(e) == RHS::ZERO)) { continue; }
+            switch (system.rhs.get(e)) {
+                case RHS::ZERO_OR_ONE: os << " == 0 or 1\n"; break;
+                case RHS::ZERO: os << " == 0\n"; break;
+                case RHS::ONE: os << " == 1\n"; break;
+            }
+        }
+        for (std::size_t i = 0; i < M - 1; ++i) {
+            const VAR value = system.p.get(i);
+            if ((value == VAR::ZERO) || (value == VAR::ONE)) {
+                assert(!p_used.test(i));
+            } else if (value == VAR::ZERO_OR_ONE) {
+                os << "p" << (i + 1) << " == 0 or 1\n";
+            } else {
+                os << "0 <= p" << (i + 1) << " <= 1\n";
+            }
+        }
+        for (std::size_t i = 0; i < N - 1; ++i) {
+            const VAR value = system.q.get(i);
+            if ((value == VAR::ZERO) || (value == VAR::ONE)) {
+                assert(!q_used.test(i));
+            } else if (value == VAR::ZERO_OR_ONE) {
+                os << "q" << (i + 1) << " == 0 or 1\n";
+            } else {
+                os << "0 <= q" << (i + 1) << " <= 1\n";
+            }
+        }
+        return os;
+    }
+
+
     constexpr void set_p_zero(var_index_t p_index) noexcept {
         assert((1 <= p_index) && (p_index <= M - 1));
         assert(p.get(p_index - 1) != VAR::ONE);
@@ -222,15 +291,15 @@ struct System {
     }
 
 
-    constexpr bool is_zero_or_one(const Term &term) const noexcept {
-        if (term == TERM_ZERO) { return true; }
+    constexpr bool is_unknown(const Term &term) const noexcept {
+        if (term == TERM_ZERO) { return false; }
         if (term.p_index) {
-            if (p.get(term.p_index - 1) == VAR::UNKNOWN) { return false; }
+            if (p.get(term.p_index - 1) == VAR::UNKNOWN) { return true; }
         }
         if (term.q_index) {
-            if (q.get(term.q_index - 1) == VAR::UNKNOWN) { return false; }
+            if (q.get(term.q_index - 1) == VAR::UNKNOWN) { return true; }
         }
-        return true;
+        return false;
     }
 
 
@@ -284,8 +353,7 @@ struct System {
         // After the end of Phase 1, we may assume that
         // the term 1 does not appear in any equation.
 
-        // Phase 2: Propagate right-hand sides to left-hand sides.
-        bool made_changes = false;
+        // Phase 2: Use right-hand sides to directly solve for variables.
         for (std::size_t e = 0; e < M + N - 1; ++e) {
             const RHS rhs_value = rhs.get(e);
             if (rhs_value == RHS::ZERO) {
@@ -325,35 +393,39 @@ struct System {
                     if (term.q_index) { set_q_one(term.q_index); }
                     return simplify();
                 }
-            } else {
-                // If an equation has the form t_1 + t_2 + ... + t_k == 0 or 1
-                // and all but one of the terms t_i are already known to be
-                // 0 or 1, then the remaining term must also be 0 or 1.
-                std::size_t unknown_index = INVALID_INDEX;
-                for (std::size_t t = 0; t < M + 1; ++t) {
-                    if (!is_zero_or_one(lhs[e][t])) {
-                        if (unknown_index != INVALID_INDEX) {
-                            unknown_index = INVALID_INDEX;
-                            break;
-                        } else {
-                            unknown_index = t;
-                        }
+            }
+        }
+
+        // Phase 3: Eliminate unknown variables using all-but-one principle.
+        bool made_changes = false;
+        for (std::size_t e = 0; e < M + N - 1; ++e) {
+            // If an equation has the form t_1 + t_2 + ... + t_k == 0 or 1
+            // and all but one of the terms t_i are already known to be
+            // 0 or 1, then the remaining term must also be 0 or 1.
+            std::size_t unknown_index = INVALID_INDEX;
+            for (std::size_t t = 0; t < M + 1; ++t) {
+                if (is_unknown(lhs[e][t])) {
+                    if (unknown_index != INVALID_INDEX) {
+                        unknown_index = INVALID_INDEX;
+                        break;
+                    } else {
+                        unknown_index = t;
                     }
                 }
-                if (unknown_index != INVALID_INDEX) {
-                    const Term term = lhs[e][unknown_index];
-                    if (term.q_index == 0) {
-                        made_changes |= set_p_zero_or_one(term.p_index);
-                    } else if (term.p_index == 0) {
-                        made_changes |= set_q_zero_or_one(term.q_index);
-                    }
+            }
+            if (unknown_index != INVALID_INDEX) {
+                const Term term = lhs[e][unknown_index];
+                if (term.q_index == 0) {
+                    made_changes |= set_p_zero_or_one(term.p_index);
+                } else if (term.p_index == 0) {
+                    made_changes |= set_q_zero_or_one(term.q_index);
                 }
             }
         }
         if (!has_unknown_variable()) { return true; }
         if (made_changes) { return simplify(); }
 
-        // Phase 3: Solve subsystems of the following form:
+        // Phase 4: Eliminate unknown variables in subsystems of the form:
         //     a + b == 0 or 1
         //     a * b == 0 or 1
         Term lone_quadratic_terms[M + N - 1];
