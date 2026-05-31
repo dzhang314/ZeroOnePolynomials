0cc35e5b551e84f9c17951f87a8bc372d3b23c91
David K. Zhang <dzhang314@gmail.com>
2026-05-31 16:50:33 -0700

    add statement of restricted conjecture

--- ZeroOnePolynomials/Conjecture.lean
+++ ZeroOnePolynomials/Conjecture.lean
@@ -1,20 +1,32 @@
 import Mathlib.Algebra.Polynomial.Degree.Defs
 import Mathlib.Data.Real.Basic
 
+open scoped Polynomial
+open Polynomial (coeff Monic X)
+
 namespace ZeroOnePolynomials
 
-def IsZeroOnePolynomial (P : Polynomial ℝ) : Prop :=
-  ∀ n : ℕ, (P.coeff n = 0) ∨ (P.coeff n = 1)
+def IsZeroOnePolynomial (P : ℝ[X]) : Prop :=
+  ∀ n : ℕ, (coeff P n = 0) ∨ (coeff P n = 1)
 
-def HasNonnegativeCoefficients (P : Polynomial ℝ) : Prop :=
-  ∀ n : ℕ, P.coeff n ≥ 0
+def HasNonnegativeCoefficients (P : ℝ[X]) : Prop :=
+  ∀ n : ℕ, coeff P n ≥ 0
 
-def ZeroOnePolynomialConjecture (P Q : Polynomial ℝ) : Prop :=
-  P.Monic →
-  Q.Monic →
-  HasNonnegativeCoefficients P →
-  HasNonnegativeCoefficients Q →
-  IsZeroOnePolynomial (P * Q) →
-  IsZeroOnePolynomial P ∧ IsZeroOnePolynomial Q
+def IsRestrictedPolynomial (P : ℝ[X]) : Prop :=
+  (Monic P) ∧ (HasNonnegativeCoefficients P) ∧ (coeff P 0 = 1)
+
+def ZeroOnePolynomialConjecture (P Q : ℝ[X]) : Prop :=
+  (Monic P) →
+  (Monic Q) →
+  (HasNonnegativeCoefficients P) →
+  (HasNonnegativeCoefficients Q) →
+  (IsZeroOnePolynomial (P * Q)) →
+  (IsZeroOnePolynomial P ∧ IsZeroOnePolynomial Q)
+
+def RestrictedConjecture (P Q : ℝ[X]) : Prop :=
+  (IsRestrictedPolynomial P) →
+  (IsRestrictedPolynomial Q) →
+  (IsZeroOnePolynomial (P * Q)) →
+  (IsZeroOnePolynomial P ∧ IsZeroOnePolynomial Q)
 
 end ZeroOnePolynomials
